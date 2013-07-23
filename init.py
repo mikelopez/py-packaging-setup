bc45c3411a9a54a0b75936e89bca106abb0701ff
Mike Lopez <dev@scidentify.info>
2013-07-23 13:21:18 -0400

    trailing and prefix slash checks and fixes

--- init.py
+++ init.py
@@ -5,10 +5,13 @@ Author: Marcos Lopez - dev@scidentify.info
 
 """
 import os
+import sys
 from termprint import termprint
 from settings import CREATE_DIRECTORY as cdir
 
 class Project:
+    destination = None
+
     def __init__(self):
         pass
 
@@ -21,5 +24,40 @@ class Project:
         s = raw_input(msg)
         return s.replace(' ','').replace('.','').replace(',','')
 
+    def remove_trailing_slash(self, dir):
+        """ Remove a trailing slash from a directory if any.
+        E.g: turns /tmp/mycoolstuff/myapp/ to: /tmp/mycoolstuff/myapp
+        """
+        if self.has_trailing_slash(dir):
+            return dir[:-1]
+
+    def has_trailing_slash(self, dir):
+        """ Check if dir string has trailing slash by checking first char"""
+        return path[:1] == '/'
+
+
+
+    def set_destination(self, path):
+        """ Check if relative or abs and set the path
+        remove any trailing slashes. Remove any prefix slashes
+        If there is an absolute path provided it will omit the settings
+        CREATE_DIRECTORY value.
+        """
+        # if provided was an absolute path
+        if sel.has_trailing_slash(path)
+            self.destination = path
+        else:
+            # should not copy to relative path cause no settins is set for project root
+            if not cdir:
+                termprint("ERROR", "No Absolute path provided and no settings, do either one. \
+                    Set your CREATE_DIRECTORY variable in settings")
+                sys.exit(1)
+            self.destination = "%s/%s" % (self.get_project_root(), path)
+        
+
+
+
     def get_project_root(self):
-        return cdir
+        """ Return the project root and strip trailng slash """
+        return self.remove_trailing_slash(cdir)
+        
--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from init import *
 if __name__ == '__main__':
     p = Project()
 
-    # ask a user appropriatelybased on settings
+    # ask a user appropriately based on settings
     if not p.get_project_root():
         termprint("", "\nProvide an absolute path or directory will be created on local directory:")
         termprint("WARNING", "\t%s" % os.getcwd())
@@ -12,8 +12,10 @@ if __name__ == '__main__':
 
     else:
         termprint("", "\nProvide a package name.")
-        termprint("", "The package will be created inside directory:")
-        termprint("INFO", "%s\n", p.get_project_root())
+        termprint("", "\nThe package will be created inside directory:")
+        termprint("INFO", "\t%s\n" % p.get_project_root())
         termprint("WARNING", "Note: By Providing an absolute path (/tmp/myapp) will omit settings")
         response = p.ask_user('\nPackage folder name (folder name): ')
-    p.set_abs_destination(response)
+        
+    if response and len(str(response)) > 5:
+        p.set_destination(response)
