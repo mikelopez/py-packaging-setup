3746d70c4cb397ba3ea4fd8744cb2d3958851598
Mike Lopez <dev@scidentify.info>
2013-07-23 12:32:37 -0400

    added requirements

--- init.py
+++ init.py
@@ -4,7 +4,19 @@ Create a python module or django pluggable app skeleton
 Author: Marcos Lopez - dev@scidentify.info
 
 """
+from settings import CREATE_DIRECTORY as cdir
 
 class Project:
+    def __init__(self):
+        pass
+
+    def __ask_user(self, msg):
+        """
+        ask a question
+        get a response
+        return clean string (no spaces, dots, commas)
+        """
+        s = raw_input(msg)
+        return s.replace(' ','').replace('.','').replace(',','')
+
     pass
-    
---
+++ settings.py
@@ -0,0 +1,4 @@
+# Use this directory by default to place the new created files
+# Using this, you will only need to provide the relative folder name
+# NOT the full path. Omit this to pass a full path.
+CREATE_DIRECTORY = None
